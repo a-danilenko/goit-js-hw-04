5d7eab4b0834247693faa27d74a9d39d4f837f72
a-danilenko <adanilenko7@gmail.com>
2020-09-04 14:59:03 +0300

    hw4_finish

--- js/task-4-02.js
+++ js/task-4-02.js
@@ -1,11 +1,25 @@
-const countProps = function (obj) {
+const isUniq = (element, index, arr) => arr.indexOf(element) === index;
+const isEven = (element) => element % 2 === 0;
+
+function filterArray(array, cb) {
     'use strict';
-    // Write code under this line
-    return Object.keys(obj).length;
-};
+    const numbers = [];
+    for (let i = 0; i < array.length; i += 1) {
+        const element = array[i];
+        const index = i;
+        // Write code under this line
+        if (cb(element, index, array)) {
+            numbers.push(element);
+        }
+    }
 
-console.log(countProps({})); // 0
+    return numbers;
+}
 
-console.log(countProps({ a: 1, b: 1 })); // 2
+const arr = [1, 2, 3, 4, 5, 1, 2, 5];
 
-console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
+console.log(filterArray(arr, isUniq));
+// [1, 2, 3, 4, 5]
+
+console.log(filterArray(arr, isEven));
+// [2, 4, 2]
